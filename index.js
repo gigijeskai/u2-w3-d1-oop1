7cd6fd10e86f2985fa682d67ca6ea057c04ea6b1
Luigi Indaco <luigindaco@gmail.com>
2022-10-10 18:23:39 +0200

    fix

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-
-//  let form = document.querySelector('form');
-//  let nome = document.querySelector('#nome');
-//  let cognome = document.querySelector('#cognome');
-//  let anni = document.querySelector('#anni');
-
-
-
 class Persona {
-    constructor (nome,cognome,anni){
+  constructor(nome, cognome, anni) {
+    this.nome = nome;
+    this.cognome = cognome;
+    this.anni = anni;
+  }
+  createTablePersone() {
+    let table = document.createElement("table");
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
+    let td = document.createElement("td");
+    tr.appendChild(td);
+    td.innerHTML += this.nome;
+    td.innerHTML += this.cognome;
+    td.innerHTML += this.anni;
 
-     this.nome = nome
-     this.cognome = cognome
-     this.anni = anni
-     }
-    createTablePersone(){
+    let persone = document.querySelector("#persone");
+    persone.appendChild(table);
+  }
+}
 
-let table = document.createElement('table')
-let tr = document.createElement('tr')
- table.appendChild(tr)
-let td = document.createElement('td')
- tr.appendChild(td)
- td.value = this.nome
-// let persone = document.querySelector('persone')
-// persone.appendChild(table)
-
-    } 
- }
-
-//  let persone = [
-//     new persona(document.querySelector('#nome').value),
-//    (document.querySelector('#cognome').value),
-//    (document.querySelector('#anni').value)
-//  ]
-
+function aggiungiPersona() {
+  let persone = new Persona(document.querySelector("#nome").value, document.querySelector("#cognome").value, document.querySelector("#anni").value);
+  persone.createTablePersone();
+}
